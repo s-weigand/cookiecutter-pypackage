b0ac9f53b1dae052b0a1aeda643d606fd452dcbd
s-weigand <s.weigand.phy@gmail.com>
2020-08-15 12:02:16 +0200

    Fixed string characters

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -19,11 +19,11 @@ test_requirements = [
 ]
 
 {%- set license_classifiers = {
-    'MIT license': 'License :: OSI Approved :: MIT License',
-    'BSD license': 'License :: OSI Approved :: BSD License',
-    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
-    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
-    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    "MIT license': 'License :: OSI Approved :: MIT License",
+    "BSD license': 'License :: OSI Approved :: BSD License",
+    "ISC license': 'License :: OSI Approved :: ISC License (ISCL)",
+    "Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License",
+    "GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
 } %}
 
 setup(
