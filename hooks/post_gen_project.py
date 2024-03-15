daf4b87e7602b406a10c636f828ec36831a8d994
s-weigand <s.weigand.phy@gmail.com>
2024-03-15 03:14:25 +0100

    👌 Use main as git init branch

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,6 +10,9 @@ PROJECT_DIRECTORY = Path().cwd()
 def run_pre_commit():
     """Init git repository, install and run pre-commit."""
     print("Initializing git repository and running pre-commit on all files.")
+    subprocess.run(
+        "git config --global init.defaultBranch main", cwd=PROJECT_DIRECTORY, shell=True
+    )
     subprocess.run("git init", cwd=PROJECT_DIRECTORY, shell=True)
     subprocess.run("git add .", cwd=PROJECT_DIRECTORY, shell=True)
     subprocess.run("pre-commit install", cwd=PROJECT_DIRECTORY, shell=True)
